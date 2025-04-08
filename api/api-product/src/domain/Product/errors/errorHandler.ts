f1d993cabc8042fb9a5d22ba04425c2cfe485328
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-04-07 21:17:46 -0300

    feat: errorHandler

---
+++ api/api-product/src/domain/Product/errors/errorHandler.ts
@@ -0,0 +1,11 @@
+// src/middlewares/errorHandler.ts
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+    console.error(err);
+
+    const status = err.statusCode || 500;
+    const message = err.message || "Erro interno no servidor";
+
+    res.status(status).json({ error: message });
+}
--- api/api-product/src/index.ts
+++ api/api-product/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import productRoutes from './routes/product.route';
+import { errorHandler } from './domain/Product/errors/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/product", productRoutes);
+
+app.use(errorHandler);
 
 export default app;
